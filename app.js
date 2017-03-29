e9e1a7b330b40557081510b947e3054082d56982
gee2789 <geeyung.li@gmail.com>
2017-03-29 17:08:27 -0400

    added code to show billQuery

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
+var bill_result1 = {}
+
 $(() => {
   $('#queryForm').on('submit', function(event){
     event.preventDefault()
     let query = $(this).find('input#query').val()
     let state = $(this).find('input#state').val()
     Bill.all(query, state)
-    .then(function(billList) {
-      let result = billList.searchresult
-       for(var element in result) {
-        var title = result[element].title
-        var bill_id = result[element].bill_id
-        var number = result[element].bill_number
-        var research_url = result[element].research_url
-        var last_action = result[element].last_action
-        }
+    .then(function({searchresult}) {
+      delete searchresult.summary
+      return Object.values(searchresult)
+    }).then((results) => {
+      let $target = $('ul.billQuery')
+      let listController = new BillListController($target, results)
+
     // $(this).find('input#firstQuery').val('')
     // $(this).find('input#state').val('')
     //want to have ready for the next search query?
   })
 })
 })
-
-// const detailedSearchApi = (billId) => {
-//   $.get(`https://api.legiscan.com/?key=1da8e9a29801464091ed4d9ba6e595f4&op=getBill&${billId}`)
-//   .then(function({searchresult}) {
-//     console.log(searchresult)
-//   }
-// }
-
-// $('.container ul').empty() || $('.container ul').innerHtml = ''
