0e9315df3c26c21ebf766592638b341bf086760f
krb3p <ryankatherineburke@gmail.com>
2017-03-28 22:00:03 -0400

    ryan_push2

---
+++ NOTES.md
@@ -0,0 +1,26 @@
+1. For each on the search query results to create new instances of the detailed
+   views (automatic query based on the search and then you create new instances).
+2. If store.bills includes then do not create (find to avoid repeating)
+
+3. Category -- need them to be objects first
+
+-- On the return you could get interesting views with a nested search
+
+--
+Additional Features
+
+1. Comments
+2. Up / down
+
+--
+
+Page for each state
+- All the categories and search (can either choose the categories and search)
+- state.categories [categories is committee names] -- categories belong to a state.
+  State name @categories = state.categories
+  *One layout and data
+
+Twitter / local news feeds with API searches for the bills
+--
+Longer term
+- Categorization across states for areas of emphasis
--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 $(() => {
   $('#queryForm').on('submit', function(event){
     event.preventDefault()
-    let query = $(this).find('input#query').val()
+    let query = $(this).find('input#firstQuery').val()
     let state = $(this).find('input#state').val()
     searchApi(query, state)
+    $(this).find('input#firstQuery').val('')
+    $(this).find('input#state').val('')
+    //want to have ready for the next search query?
   })
 })
 
 const searchApi = (query, state) => {
   $.get(`https://api.legiscan.com/?key=1da8e9a29801464091ed4d9ba6e595f4&op=search&state=${state}&query=${query}`)
   .then(function({searchresult}) {
-    console.log(searchresult)
+    for(i = 0; i < searchresult.length; i++) {
+      let title = searchresult[i].title
+      let number = searchresult[i].bill_number }
+      //is this going to work since it's a hash not an array?
+    //need to add functionality to check if the bill already exists in memory
   })
 }
+
+// const detailedSearchApi = (billId) => {
+//   $.get(`https://api.legiscan.com/?key=1da8e9a29801464091ed4d9ba6e595f4&op=getBill&${billId}`)
+//   .then(function({searchresult}) {
+//     console.log(searchresult)
+//   }
+// }
