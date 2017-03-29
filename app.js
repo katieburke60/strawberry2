9a89f5544cc58aeb4f016117f938ee0aee4e53db
gee2789 <geeyung.li@gmail.com>
2017-03-29 09:06:09 -0400

    added states array and bill_result for initial mapping

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var bill_result1 = {}
+
 $(() => {
   $('#queryForm').on('submit', function(event){
     event.preventDefault()
@@ -10,6 +12,12 @@ $(() => {
 const searchApi = (query, state) => {
   $.get(`https://api.legiscan.com/?key=1da8e9a29801464091ed4d9ba6e595f4&op=search&state=${state}&query=${query}`)
   .then(function({searchresult}) {
-    console.log(searchresult)
+    bill_result1 = searchresult
   })
 }
+
+const stateArray = new Array("AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY")
+
+bill_result1.map(() => {
+
+})
